2c03f6ea81d523b30f5b2605aaadf9e1d6f8ba2e
Fred Alisson da Silva Sousa <fredalissonsousa@gmail.com>
2025-03-03 21:42:32 -0300

    refact: established API rate limit. Now, fetch data and fetch batch data retrive data for HISTORICAL_OPTIONS and OVERVIEW

--- src/bronze/alpha_vantage_api.py
+++ src/bronze/alpha_vantage_api.py
@@ -1,40 +1,53 @@
 from typing import Optional, Dict, List
 from utils.config import bronze_logger
 from utils.api_utils import APIUtils
+import time
 
 class AlphaVantageAPIFetcher(APIUtils):
     """
-    A class to fetch historical options data from the Alpha Vantage API.
+    A class to fetch historical options and overview data from the Alpha Vantage API.
 
     This class inherits from APIUtils and provides methods to prepare query parameters,
     fetch data for a single symbol, and batch fetch data for multiple symbols.
     """
 
     BASE_URL: str = "https://www.alphavantage.co/query"
+    RATE_LIMIT_DELAY: int = 15
+    SUPPORTED_FUNCTIONS: List[str] = ["HISTORICAL_OPTIONS", "OVERVIEW"]
     
     @classmethod
-    def _setup_params(cls, symbol: str) -> Dict:
+    def _setup_params(cls, symbol: str, function: str) -> Dict:
         """
         Prepare the parameters required for the Alpha Vantage API request.
 
         Constructs a dictionary of parameters needed by the Alpha Vantage API, including the function type,
-        symbol, API key, and output size.
+        symbol, API key, and output size if the function is HISTORICAL_OPTION.
 
         Args:
             symbol (str): The stock symbol for which to fetch data.
+            function (str): Function to get data, can be HISTORICAL_OPTION or OVERVIEW.
 
         Returns:
             Dict[str, str]: A dictionary of parameters to be sent with the API request.
         """
-        return {
-            "function": "HISTORICAL_OPTIONS",
+        if function not in cls.SUPPORTED_FUNCTIONS:
+            raise ValueError(
+                f"Unsupported function: {function}. Valid option: {cls.SUPPORTED_FUNCTIONS}"
+            )
+        api_key = cls._get_api_key(name_key="API_ALPHA_VANTAGE_KEY")
+        params: dict = {
+            "function": function,
             "symbol": symbol,
-            "apikey": cls._get_api_key(name_key="API_ALPHA_VANTAGE_KEY"),
-            "outputsize": "full"
+            "apikey": api_key,
         }
+        
+        if function == "HISTORICAL_OPTIONS":
+            params["outputsize"] = "full"
+        
+        return params
 
     @classmethod
-    def fetch_data(cls, symbol: str) -> Optional[Dict]:
+    def fetch_data(cls, symbol: str, function: str) -> Optional[Dict]:
         """
         Fetch historical options data for the given symbol from the Alpha Vantage API.
 
@@ -43,35 +56,40 @@ class AlphaVantageAPIFetcher(APIUtils):
 
         Args:
             symbol (str): The stock symbol for which to fetch data.
+            function (str): Function to get data, can be HISTORICAL_OPTION or OVERVIEW.
 
         Returns:
             Optional[Dict]: A dictionary containing the API data if successful; otherwise, None.
         """
-        params = cls._setup_params(symbol = symbol)
+        params = cls._setup_params(symbol, function)
         raw_data = cls._fetch_data(params = params)
         validate_data = super()._validate_response(data = raw_data, symbol = symbol)
         
         return validate_data
         
     @classmethod
-    def fetch_batch_data(cls, symbols: List[str]) -> Dict:
+    def fetch_batch_data(cls, symbols: List[str], function: str) -> Dict[str, Optional[Dict]]:
         """
         Fetch historical options data for multiple symbols from the Alpha Vantage API.
 
-        Iterates over a list of symbols, fetches the data for each symbol, and compiles the results
-        into a dictionary mapping each symbol to its respective data.
+        Iterates over a list of symbols, fetches the data for each symbol based on the function, and compiles the results
+        into a dictionary mapping each symbol to its respective data and their index for rate limit.
 
         Args:
             symbols (List[str]): A list of stock symbols for which to fetch data.
-
+            function (str): Function to get data, can be HISTORICAL_OPTION or OVERVIEW.
         Returns:
             Dict[str, Optional[Dict]]: A dictionary where keys are symbols and values are the corresponding
             data dictionaries. If data retrieval fails for a symbol, its value will be None.
         """
         results: dict = {}
-        for symbol in symbols:
-            bronze_logger.info(f"Fetching data for symbol: {symbol}")
-            data = cls.fetch_data(symbol)
-            results[symbol] = data
+        for idx, symbol in enumerate(symbols):
+            bronze_logger.info(f"Processing {symbol} ({idx+1}/{len(symbols)})")
+            results[symbol] = cls.fetch_data(symbol, function)
+            
+            if (idx + 1) % 5 == 0:
+                bronze_logger.debug(f"Rate limit throttle: Waiting {cls.RATE_LIMIT_DELAY}s")
+                time.sleep(cls.RATE_LIMIT_DELAY)
             
         return results
+    
